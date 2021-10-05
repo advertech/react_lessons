d58bb48754827aa18c02e6c3c7e51bb5da0e0830
advertech <pavel@advertech.ee>
2021-10-05 21:46:49 +0300

    Same code on JSX

--- LES2/wellcome React/index.js
+++ LES2/wellcome React/index.js
@@ -1,19 +1,35 @@
 // Wellcome to React LES 2 Part 1
 
 
-const render = React.createElement('div', null,
-    React.createElement('header', null,
-        React.createElement('img', {
-            src: 'Logo.svg',
-            alt: 'Logo react',
-            className: 'logo'
-        }),
-        React.createElement('h1', null, 'Hello React')
-    ),
-    React.createElement('main', null,
-        React.createElement('p', null, 'Java Scrippt Lessons ')
-    )
+// const render = React.createElement('div', null,
+//     React.createElement('header', null,
+//         React.createElement('img', {
+//             src: 'Logo.svg',
+//             alt: 'Logo react',
+//             className: 'logo'
+//         }),
+//         React.createElement('h1', null, 'Hello React')
+//     ),
+//     React.createElement('main', null,
+//         React.createElement('p', null, 'Java Scrippt Lessons ')
+//     )
 
-)
+// )
 
-ReactDOM.render(render, document.getElementById('root'))
+// ReactDOM.render(render, document.getElementById('root'))
+
+// тоже самое на JSD  - надо прогнать через Бабель
+
+const renderJSX = (
+    <div>
+        <header>
+            <img className='logo' src="logo.svg" alt="ReactLogo" />
+            <h1>Priver React</h1>
+        </header>
+        <main>
+            <p> </p>
+        </main>
+    </div>
+);
+
+ReactDOM.render(renderJSX, document.getElementById('root'))
