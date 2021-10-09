75c314e904d26e47dcfa98dc34b292910ede4d6c
advertech <pavel@advertech.ee>
2021-10-09 14:03:22 +0300

    код переписан на классовый компонент

--- LES-1-ES6/comp.js
+++ LES-1-ES6/comp.js
@@ -13,20 +13,23 @@ const allertHelo = () => {
 
 const Hello = props => <p style={red} onClick={allertHelo}>Hello {props.name}</p>;
 
-const RenderJSX = () => (
-    <div>
-        <header>
-            <img className="logo" src="logo.svg" alt="react logo" />
-            <h1> Hello REACT</h1>
-        </header>
-        <main>
-            <p>Same code on JSX [advertech]</p>
-            <Hello name="Maks" />
-            <Hello name="Alex" />
-            <Hello name="John" />
-        </main>
-    </div>
+class RenderJSX extends React.Component {
+    render() {
+        return (
+            <div>
+                <header>
+                    <img className="logo" src="logo.svg" alt="react logo" />
+                    <h1> Hello REACT</h1>
+                </header>
+                <main>
+                    <p>Same code on JSX [advertech]</p>
+                    <Hello name="Maks" />
+                    <Hello name="Samanta" />
+                    <Hello name="John" />
+                </main>
+            </div>)
+    }
+}
 
-)
 
 ReactDOM.render(<RenderJSX />, document.getElementById('root'))
